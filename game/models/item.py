d6577ed3c1e63dd365bc74bc70ba528620f469ab
Christopher Childress <chris.childress@stratusgrid.com>
2024-10-07 23:49:42 -0400

    feat: support item/adventurer compatibility

--- game/models/item.py
+++ game/models/item.py
@@ -1,9 +1,16 @@
 class Item:
-    def __init__(self, name, type, slot, attributes=None):
+    def __init__(self, name, type, slot, attributes=None, compatible_classes=None, compatible_alignments=None):
         self.name = name
         self.type = type
         self.slot = slot
         self.attributes = attributes or {}
+        self.compatible_classes = compatible_classes or []
+        self.compatible_alignments = compatible_alignments or []
+
+    def is_compatible(self, adventurer):
+        class_compatible = not self.compatible_classes or adventurer.char_class in self.compatible_classes
+        alignment_compatible = not self.compatible_alignments or adventurer.alignment in self.compatible_alignments
+        return class_compatible and alignment_compatible
 
     def to_dict(self):
         return {
@@ -11,6 +18,8 @@ class Item:
             'type': self.type,
             'slot': self.slot,
             'attributes': self.attributes,
+            'compatible_classes': self.compatible_classes,
+            'compatible_alignments': self.compatible_alignments
         }
 
     @classmethod
@@ -19,5 +28,7 @@ class Item:
             name=data['name'],
             type=data['type'],
             slot=data['slot'],
-            attributes=data.get('attributes', {})
+            attributes=data.get('attributes', {}),
+            compatible_classes=data.get('compatible_classes', []),
+            compatible_alignments=data.get('compatible_alignments', [])
         )
